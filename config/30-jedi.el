5685b296da7346f20007e66994f9293df60f062f
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2015-03-02 22:46:45 +0900

    0302 checked

--- config/30-jedi.el
+++ config/30-jedi.el
@@ -1,10 +1,10 @@
 ;;-*- coding: utf-8 -*-
 
-;; M-x package-install jedi
 ;; M-x jedi:install-server
 
-(my-require 'jedi)
-(setq jedi:complete-on-dot t)
-(add-hook 'python-mode-hook 'jedi:setup)
-
-(push '("*jedi:doc*" :noselect t) popwin:special-display-config)
+(el-get-bundle jedi
+  (add-hook 'python-mode-hook 'jedi:setup)
+  (with-eval-after-load-feature 'jedi
+    (setq jedi:complete-on-dot t)
+    (with-eval-after-load-feature 'popwin
+      (push '("*jedi:doc*" :noselect t) popwin:special-display-config))))
