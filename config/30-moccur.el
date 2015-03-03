2fdcb1ce82080876806a94fb91e1cc4f752f23ba
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2015-03-03 23:14:13 +0900

    Remove delayed loading

--- config/30-moccur.el
+++ config/30-moccur.el
@@ -1,6 +1,6 @@
 ;;-*- coding: utf-8 -*-
 
-(el-get-bundle color-moccur
-  (with-eval-after-load-feature 'color-moccur
-    (add-to-list 'dmoccur-exclusion-mask "\\.DS_Store")
-    (setq moccur-split-word t)))
+(el-get-bundle color-moccur)
+(require 'color-moccur)
+(add-to-list 'dmoccur-exclusion-mask "\\.DS_Store")
+(setq moccur-split-word t)
