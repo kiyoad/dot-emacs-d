905af4596e40c45486b6049e4968d9df37e344e7
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2015-03-10 23:31:05 +0900

    Delete overlapped function

--- config/30-highlight-symbol.el
+++ config/30-highlight-symbol.el
@@ -1,7 +1,11 @@
 ;;-*- coding: utf-8 -*-
 
-(el-get-bundle highlight-symbol
-  (global-set-key (kbd "C-M-SPC") 'highlight-symbol-at-point))
-;; (global-set-key [f3] 'highlight-symbol-next)
-;; (global-set-key [(shift f3)] 'highlight-symbol-prev)
+(el-get-bundle highlight-symbol)
+(require 'highlight-symbol)
+(setq-default highlight-symbol-mode t)
+(global-set-key (kbd "C-M-SPC") 'highlight-symbol-at-point)
+(global-set-key (kbd "M-(") 'highlight-symbol-prev)
+(global-set-key (kbd "M-)") 'highlight-symbol-next)
 ;; (global-set-key [(meta f3)] 'highlight-symbol-query-replace)
+(custom-set-faces
+ '(highlight-symbol-face ((t (:background "#401000")))))
