1196ac37511be7840c068267a2c111112d41d9c1
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2015-03-21 22:27:48 +0900

    Remove try-expand-all-abbrevs

--- config/30-hippie-expand.el
+++ config/30-hippie-expand.el
@@ -3,7 +3,7 @@
 (setq hippie-expand-try-functions-list
   '(try-complete-file-name-partially
     try-complete-file-name
-    try-expand-all-abbrevs
+    ;; try-expand-all-abbrevs
     ;; try-expand-list
     ;; try-expand-line
     try-expand-dabbrev
