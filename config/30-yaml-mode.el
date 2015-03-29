606ed51e3e72fa95a06e247da2aa11d55cb8f3be
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2015-03-29 22:58:14 +0900

    Add yaml-mode

---
+++ config/30-yaml-mode.el
@@ -0,0 +1,7 @@
+;;-*- coding: utf-8 -*-
+
+(el-get-bundle yaml-mode
+  (autoload 'yaml-mode "yaml-mode"
+    "Major mode for editing YAML files" t)
+  (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+  (add-to-list 'auto-mode-alist '("\\.yaml$" . yaml-mode)))
