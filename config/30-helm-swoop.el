28db285ed56abe3f1ac598a8743b05c274d0933e
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2015-09-02 22:17:17 +0900

    Meaning isearch-regexp is reversed

--- config/30-helm-swoop.el
+++ config/30-helm-swoop.el
@@ -19,4 +19,12 @@
 
 (with-eval-after-load "helm-swoop"
   (define-key helm-swoop-map (kbd "C-s") 'helm-next-line)
-  (define-key helm-swoop-map (kbd "C-r") 'helm-previous-line))
+  (define-key helm-swoop-map (kbd "C-r") 'helm-previous-line)
+  (defun helm-swoop-from-isearch ()
+    "Invoke `helm-swoop' from isearch."
+    (interactive)
+    (let (($query (if isearch-regexp
+                      (regexp-quote isearch-string)
+                    isearch-string)))
+      (isearch-exit)
+      (helm-swoop :$query $query))))
