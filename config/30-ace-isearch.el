779866b6b0e68d0a7475a92b737f617b867d66b4
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2015-09-05 22:27:30 +0900

    Set some ace-isearch timers

--- config/30-ace-isearch.el
+++ config/30-ace-isearch.el
@@ -4,6 +4,8 @@
 (el-get-bundle ace-isearch)
 (require 'ace-isearch)
 (global-ace-isearch-mode 1)
-
+(setq ace-isearch-jump-delay 0.5)
+(setq ace-isearch-func-delay 2.0)
+(setq ace-isearch-input-length 6)
 (el-get-bundle isearch-dabbrev
   (define-key isearch-mode-map (kbd "C-i") 'isearch-dabbrev-expand))
