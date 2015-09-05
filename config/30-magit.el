2eff546eca6328f2c59329b8ec3d82f494c983c1
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2015-09-05 21:46:35 +0900

    Remove some old magit settngs

--- config/30-magit.el
+++ config/30-magit.el
@@ -6,22 +6,6 @@
   (global-set-key (kbd "C-x v z") 'magit-stash)
   (global-set-key (kbd "C-x v B") 'magit-branch-manager)
   (with-eval-after-load-feature 'magit
-    (define-key magit-mode-map (kbd "M-l") 'l-all)
-    (define-key magit-status-mode-map (kbd "q") 'my/magit-quit-session)))
+    (define-key magit-mode-map (kbd "M-l") 'l-all)))
 (el-get-bundle mo-git-blame
   (global-set-key (kbd "C-x v g") 'mo-git-blame-current))
-
-;; 新しい magit向け magit-statusの設定
-;; http://d.hatena.ne.jp/syohex/20130904/1378310201
-(defadvice magit-status (around magit-fullscreen activate)
-  (window-configuration-to-register :magit-fullscreen)
-  ad-do-it
-  (delete-other-windows))
-
-(defun my/magit-quit-session ()
-  (interactive)
-  (kill-buffer)
-  (jump-to-register :magit-fullscreen))
-
-(defadvice git-commit-commit (after move-to-magit-buffer activate)
-  (delete-window))
