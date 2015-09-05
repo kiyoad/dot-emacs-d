d43b08ca2e5a0b60c087e8a1881e219614505ad9
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2015-09-05 22:15:58 +0900

    Change old magit commands

--- config/30-magit.el
+++ config/30-magit.el
@@ -2,8 +2,8 @@
 
 (el-get-bundle magit
   (global-set-key (kbd "C-x v d") 'magit-status)
-  (global-set-key (kbd "C-x v L") 'magit-key-mode-popup-logging)
+  (global-set-key (kbd "C-x v L") 'magit-log-popup)
   (global-set-key (kbd "C-x v z") 'magit-stash)
-  (global-set-key (kbd "C-x v B") 'magit-branch-manager))
+  (global-set-key (kbd "C-x v B") 'magit-show-refs-popup))
 (el-get-bundle mo-git-blame
   (global-set-key (kbd "C-x v g") 'mo-git-blame-current))
