76374e3479aedf209f7096959e89fda1b5fdba1e
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2015-10-03 11:54:56 +0900

    Disable global highlight-symbol-mode

--- config/30-highlight-symbol.el
+++ config/30-highlight-symbol.el
@@ -1,11 +1,19 @@
 ;;-*- coding: utf-8 -*-
 
-(setq highlight-symbol-mode t)
+;; (setq highlight-symbol-mode t)
 (el-get-bundle highlight-symbol)
 (require 'highlight-symbol)
-(global-set-key (kbd "C-M-SPC") 'highlight-symbol-at-point)
+;; (global-set-key (kbd "C-M-SPC") 'highlight-symbol-at-point)
 (global-set-key (kbd "M-(") 'highlight-symbol-prev)
 (global-set-key (kbd "M-)") 'highlight-symbol-next)
 ;; (global-set-key [(meta f3)] 'highlight-symbol-query-replace)
 (custom-set-faces
  '(highlight-symbol-face ((t (:background "#0000FF")))))
+
+(add-hook 'emacs-lisp-mode-hook 'highlight-symbol-mode)
+(add-hook 'lisp-interaction-mode-hook 'highlight-symbol-mode)
+(add-hook 'lisp-mode-hook 'highlight-symbol-mode)
+(add-hook 'ielm-mode-hook 'highlight-symbol-mode)
+(add-hook 'python-mode-hook 'highlight-symbol-mode)
+(add-hook 'c-mode-common-hook 'highlight-symbol-mode)
+
