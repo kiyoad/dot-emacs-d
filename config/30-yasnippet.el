92101c37f57682e82b700b6ed994194e42ec7d76
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2015-10-12 22:47:29 +0900

    Add yasnippet

---
+++ config/30-yasnippet.el
@@ -0,0 +1,9 @@
+;;-*- coding: utf-8 -*-
+
+;; http://rubikitch.com/2015/10/05/helm-c-yasnippet/
+
+(el-get-bundle yasnippet)
+(el-get-bundle elpa:helm-c-yasnippet
+  (setq helm-yas-space-match-any-greedy t))
+(global-set-key (kbd "C-c y") 'helm-yas-complete)
+(yas-global-mode 1)
