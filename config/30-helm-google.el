de0fa25442b5f1c5741d955bbeb016d3570c74b3
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2015-10-20 21:23:10 +0900

    Add helm-google

---
+++ config/30-helm-google.el
@@ -0,0 +1,11 @@
+;;-*- coding: utf-8 -*-
+
+;; http://rubikitch.com/2015/10/14/helm-google/
+
+(el-get-bundle helm-google
+  (require 'helm-google)
+  (setq helm-google-tld "co.jp")
+ ;;; Google側のサイトデザイン変更で動かなくなったらAPI検索に切替える
+  ;; (setq helm-google-search-function 'helm-google-api-search)
+ ;;; ブラウザをEWWにすればEmacs内で完結する
+  (setq browse-url-browser-function 'eww-browse-url))
