8ada8b444005248ecd229fbf43346609b3fee735
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2015-10-23 22:43:20 +0900

    Add 2 packages

---
+++ config/30-ietf-docs.el
@@ -0,0 +1,5 @@
+;;-*- coding: utf-8 -*-
+
+;; http://rubikitch.com/2015/10/19/ietf-docs/
+
+(el-get-bundle ietf-docs)
---
+++ config/30-shell-pop.el
@@ -0,0 +1,6 @@
+;;-*- coding: utf-8 -*-
+
+;; http://rubikitch.com/2015/06/16/shell-pop/
+
+(el-get-bundle shell-pop
+  (global-set-key (kbd "C-c c") 'shell-pop))
