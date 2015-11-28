951422fbfa361eedfc3dec0b3ac44d119461641f
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2015-11-29 00:58:46 +0900

    Change linum format

--- config/30-linum-mode.el
+++ config/30-linum-mode.el
@@ -4,6 +4,8 @@
 ;; (set-face-attribute 'linum nil
 ;;                     :foreground "#800"
 ;;                     :height 0.9)
-(setq linum-format "%5d ")
-
-(add-hook 'prog-mode-hook 'linum-mode)
+(setq linum-format "%5d")
+(add-hook 'prog-mode-hook
+          '(lambda ()
+             (linum-mode 1)
+             (set-face-attribute 'linum nil :height 0.75)))
