8ca8dd546b1ce1ab96e1d23156cec18c0284d7f8
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2015-11-29 21:56:21 +0900

    Change helm-c-yasnippet source

--- config/30-yasnippet.el
+++ config/30-yasnippet.el
@@ -3,7 +3,7 @@
 ;; http://rubikitch.com/2015/10/05/helm-c-yasnippet/
 
 (el-get-bundle yasnippet)
-(el-get-bundle elpa:helm-c-yasnippet
+(el-get-bundle emacs-jp/helm-c-yasnippet
   (setq helm-yas-space-match-any-greedy t))
 (global-set-key (kbd "C-c y") 'helm-yas-complete)
 (yas-global-mode 1)
