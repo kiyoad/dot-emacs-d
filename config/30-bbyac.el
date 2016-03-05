046f88d07ad66b54a7b3d6ef552dcae3fe1df175
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2016-03-05 23:52:34 +0900

    Split 30-bbyac and create 20-ido-vertical-mode

--- config/30-bbyac.el
+++ config/30-bbyac.el
@@ -3,14 +3,9 @@
 ;; http://rubikitch.com/2015/03/12/bbyac/
 
 (el-get-bundle browse-kill-ring)
-(el-get-bundle ido-vertical-mode)
 (el-get-bundle baohaojun/bbyac)
 (require 'browse-kill-ring)
-(require 'ido-vertical-mode)
 (require 'bbyac)
-(setq ido-max-window-height 0.75)
-(setq ido-enable-flex-matching t)
-(ido-vertical-mode 1)
 
 (setq bbyac-max-chars 99999)
 (defun bbyac--display-matches--use-ido (orig strlist)
@@ -21,4 +16,3 @@
         (t (apply orig strlist))))
 (advice-add 'bbyac--display-matches :around 'bbyac--display-matches--use-ido)
 (bbyac-global-mode 1)
-(setq ido-vertical-define-keys 'C-n-C-p-up-down-left-right)
