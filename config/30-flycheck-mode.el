294b876425aba797da66cd4fb27e1955b9d6f0c6
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2016-05-05 17:45:24 +0900

    Enable flycheck-pos-tip-mode after load flycheck

--- config/30-flycheck-mode.el
+++ config/30-flycheck-mode.el
@@ -2,14 +2,10 @@
 
 ;; sudo pip install flake8
 
+(el-get-bundle syohex/pos-tip)
+(el-get-bundle flycheck-pos-tip)
 (el-get-bundle flycheck
   (add-hook 'python-mode-hook 'flycheck-mode)
   (with-eval-after-load-feature 'flycheck
+    (flycheck-pos-tip-mode)
     (custom-set-variables '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages))))
-(el-get-bundle syohex/pos-tip)
-(el-get-bundle flycheck-pos-tip
-  (defun my-flycheck-pos-tip-show (messages)
-    (pos-tip-show (mapconcat #'identity messages "\n")
-                  '("white" . "gray30")))
-  (with-eval-after-load-feature 'flycheck-pos-tip
-    (setq flycheck-pos-tip-show-function #'my-flycheck-pos-tip-show)))
