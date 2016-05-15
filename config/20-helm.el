9e7e369c248107d4c54585ed5f7889d8c58e5d22
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2016-05-15 21:31:15 +0900

    Remove unused settings

--- config/20-helm.el
+++ config/20-helm.el
@@ -10,22 +10,12 @@
 (require 'helm-files)
 
 (require 'bind-key)
-;;(bind-key "M-x"     'helm-M-x)
 (bind-key "C-x C-z" 'helm-resume)
 (setq max-specpdl-size 3000)            ; for imenu
 (bind-key "C-x C-y" 'helm-semantic-or-imenu)
 (bind-key "M-y"     'helm-show-kill-ring)
 (bind-key "C-x b"   'helm-multi-files)
 (bind-key "C-c C-<SPC>" 'helm-all-mark-rings)
-(bind-key "C-h" 'delete-backward-char helm-map)
-
-(bind-key "C-h" 'delete-backward-char           helm-find-files-map)
-(bind-key "TAB" 'helm-execute-persistent-action helm-find-files-map)
-(bind-key "TAB" 'helm-execute-persistent-action helm-read-file-map)
-(defadvice helm-ff-kill-or-find-buffer-fname (around execute-only-if-exist activate)
-  "Execute command only if CANDIDATE exists"
-  (when (file-exists-p candidate)
-        ad-do-it))
 
 (defun my-switch-to-buffer-other-window ()
   (interactive)
