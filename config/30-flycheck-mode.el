5882bf8c4b073c04495c6d94f2cef77b8d7f9a50
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2016-05-15 21:40:06 +0900

    Always run the hook(add-hook)

--- config/30-flycheck-mode.el
+++ config/30-flycheck-mode.el
@@ -4,8 +4,8 @@
 
 (el-get-bundle syohex/pos-tip)
 (el-get-bundle flycheck-pos-tip)
-(el-get-bundle flycheck
-  (add-hook 'python-mode-hook 'flycheck-mode)
-  (with-eval-after-load-feature 'flycheck
-    (flycheck-pos-tip-mode)
-    (custom-set-variables '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages))))
+(el-get-bundle flycheck)
+(add-hook 'python-mode-hook 'flycheck-mode)
+(with-eval-after-load-feature 'flycheck
+  (flycheck-pos-tip-mode)
+  (custom-set-variables '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
