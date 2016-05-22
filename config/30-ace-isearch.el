3acc0c3760974a905410e6eee7b0bd2c8e942912
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2016-05-22 21:11:09 +0900

    Revert ace-isearch-input-length setting

--- config/30-ace-isearch.el
+++ config/30-ace-isearch.el
@@ -6,7 +6,7 @@
 (global-ace-isearch-mode 1)
 (setq ace-isearch-jump-delay 0.5)
 (setq ace-isearch-func-delay 2.0)
-(setq ace-isearch-input-length 64)
+;; (setq ace-isearch-input-length 64)
 (require 'bind-key)
 (el-get-bundle isearch-dabbrev
   (bind-key "C-i" 'isearch-dabbrev-expand isearch-mode-map))
