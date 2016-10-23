b5eac023d391f46a364708120378e1a419c559df
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2016-10-23 12:54:44 +0900

    Add patch for 25.1
    The behavior of the buffer creation of the (shell) has changed.

--- config/30-shell-pop.el
+++ config/30-shell-pop.el
@@ -5,3 +5,13 @@
 (require 'bind-key)
 (require 'shell-pop)
 (bind-key "C-c c" 'shell-pop)
+
+(defun shell-pop--switch-to-shell-buffer (index)
+  (let ((bufname (shell-pop--shell-buffer-name index)))
+    (if (get-buffer bufname)
+        (switch-to-buffer bufname)
+      (switch-to-buffer bufname)
+      (shell bufname)
+      (shell-pop--set-exit-action))
+    (setq shell-pop-last-shell-buffer-name bufname
+          shell-pop-last-shell-buffer-index index)))
