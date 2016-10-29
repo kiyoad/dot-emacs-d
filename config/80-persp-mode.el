2f576e99d93b4cf0fee0701a45af36020e82fd59
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2016-10-29 18:08:03 +0900

    Remove unused hook and add persp-set-ido-hooks for helm

--- config/80-persp-mode.el
+++ config/80-persp-mode.el
@@ -3,23 +3,13 @@
 ;; http://rubikitch.com/2015/02/13/persp-mode/
 
 (require 'persp-mode)
+(setq persp-set-ido-hooks t)
 (persp-mode 1)
-(defun persp-register-buffers-on-create ()
-  (interactive)
-  (dolist (bufname (condition-case _
-                       (helm-comp-read
-                        "Buffers: "
-                        (mapcar 'buffer-name (buffer-list))
-                        :must-match t
-                        :marked-candidates t)
-                     (quit nil)))
-    (persp-add-buffer (get-buffer bufname))))
-(add-hook 'persp-activated-hook 'persp-register-buffers-on-create)
 
 (add-to-list 'persp-filter-save-buffers-functions
              #'(lambda (b) (string-equal "ipa-mode" (buffer-local-value 'major-mode b))))
-(add-to-list 'persp-filter-save-buffers-functions
-             #'(lambda (b) (string-equal "dired-mode" (buffer-local-value 'major-mode b))))
+;; (add-to-list 'persp-filter-save-buffers-functions
+;;              #'(lambda (b) (string-equal "dired-mode" (buffer-local-value 'major-mode b))))
 (add-to-list 'persp-filter-save-buffers-functions
              #'(lambda (b) (let ((name (buffer-local-value 'buffer-file-name b)))
                              (and (stringp name) (string-match "/COMMIT_EDITMSG$" name)))))
