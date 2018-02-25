4fa5b5b421c9f09e241629e263586d72ccd4127d
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-02-25 14:12:45 +0900

    Use avy instead of ace-jump-mode

--- init.el
+++ init.el
@@ -17,7 +17,7 @@
  '(flycheck-display-errors-function (function flycheck-pos-tip-error-messages))
  '(package-selected-packages
    (quote
-    (ddskk scratch-pop japanese-holidays bm helm smart-cursor-color prompt-text ietf-docs hiwin go-errcheck go-direx direx go-eldoc helm-go-package mo-git-blame bbyac helm-c-yasnippet flycheck-pos-tip ace-isearch auto-complete browse-kill-ring color-moccur dockerfile-mode eldoc-extension fill-column-indicator fold-dwim go-mode google-translate helm-ag helm-google helm-swoop highlight-symbol ido-vertical-mode jedi json-mode lispxmp magit markdown-mode migemo pangu-spacing popwin pos-tip recentf-ext redo+ shell-pop smart-newline smart-tab smartparens smex which-key yafolding yaml-mode zop-to-char yasnippet zenburn-theme shrink-whitespace persp-mode isearch-dabbrev init-loader helm-bm exec-path-from-shell easy-kill bind-key auto-async-byte-compile))))
+    (avy ddskk scratch-pop japanese-holidays bm helm smart-cursor-color prompt-text ietf-docs hiwin go-errcheck go-direx direx go-eldoc helm-go-package mo-git-blame bbyac helm-c-yasnippet flycheck-pos-tip ace-isearch auto-complete browse-kill-ring color-moccur dockerfile-mode eldoc-extension fill-column-indicator fold-dwim go-mode google-translate helm-ag helm-google helm-swoop highlight-symbol ido-vertical-mode jedi json-mode lispxmp magit markdown-mode migemo pangu-spacing popwin pos-tip recentf-ext redo+ shell-pop smart-newline smart-tab smartparens smex which-key yafolding yaml-mode zop-to-char yasnippet zenburn-theme shrink-whitespace persp-mode isearch-dabbrev init-loader helm-bm exec-path-from-shell easy-kill bind-key auto-async-byte-compile))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
