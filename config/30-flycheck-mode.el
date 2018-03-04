0ba6a56fb620d77ceea0ff63bc68f5c7ccc2be74
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-03-04 16:30:00 +0900

    Use python3 instead of python2

--- config/30-flycheck-mode.el
+++ config/30-flycheck-mode.el
@@ -5,7 +5,14 @@
 (require 'pos-tip)
 (require 'flycheck-pos-tip)
 (require 'flycheck)
-(add-hook 'python-mode-hook 'flycheck-mode)
+
+(defun my-use-python3-instead-of-python2 (x)
+  (cond ((string= x "python") "python3")
+        (t x)))
+
 (with-eval-after-load 'flycheck
+  (advice-add 'flycheck-checker-default-executable :filter-return #'my-use-python3-instead-of-python2)
   (flycheck-pos-tip-mode)
   (custom-set-variables '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
