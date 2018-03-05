957699f9b14421232a6d8bc492beba266ccdb1b7
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-03-05 21:00:43 +0900

    Remove unneeded setting

--- config/30-flycheck-mode.el
+++ config/30-flycheck-mode.el
@@ -1,18 +1,24 @@
-;;-*- coding: utf-8 -*-
+;;; Package --- summary
+;;; -*- coding: utf-8 -*-
+;;; Commentary:
+;;; Code:
 
-;; sudo pip install flake8
+;;; C-c ! v runs the command flycheck-verify-setup
 
 (require 'pos-tip)
 (require 'flycheck-pos-tip)
 (require 'flycheck)
 
 (defun my-use-python3-instead-of-python2 (x)
+  "Replace input (as X)."
   (cond ((string= x "python") "python3")
         (t x)))
 
 (with-eval-after-load 'flycheck
   (advice-add 'flycheck-checker-default-executable :filter-return #'my-use-python3-instead-of-python2)
-  (flycheck-pos-tip-mode)
-  (custom-set-variables '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))
+  (flycheck-pos-tip-mode))
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
+
+(provide '30-flycheck-mode)
+;;; 30-flycheck-mode.el ends here
