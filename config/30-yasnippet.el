3a53c2ce49f45834f8c498e38980f31d41bd7958
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-03-10 21:23:55 +0900

    Add some config for yasnippet

--- config/30-yasnippet.el
+++ config/30-yasnippet.el
@@ -1,10 +1,24 @@
-;;-*- coding: utf-8 -*-
+;;; Package --- summary
+;;; -*- coding: utf-8 -*-
+;;; Commentary:
+;;; Code:
 
 ;; http://rubikitch.com/2015/10/05/helm-c-yasnippet/
 
 (require 'yasnippet)
+(define-key yas-minor-mode-map (kbd "<tab>") nil)
+(define-key yas-minor-mode-map (kbd "TAB") nil)
+
+(add-to-list 'hippie-expand-try-functions-list
+             'yas-hippie-try-expand)
+
 (require 'helm-c-yasnippet)
 (setq helm-yas-space-match-any-greedy t)
+
 (require 'bind-key)
 (bind-key "C-c y" 'helm-yas-complete)
+
 (yas-global-mode 1)
+
+(provide '30-yasnippet)
+;;; 30-yasnippet.el ends here
