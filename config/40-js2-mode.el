93e200717af1c1825c12dbbfc72dccfff389dbcf
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-03-10 13:05:05 +0900

    Change indent level and semi warning in js2-mode

--- config/40-js2-mode.el
+++ config/40-js2-mode.el
@@ -3,6 +3,8 @@
 ;;; Commentary:
 ;;; Code:
 
+(setq js-indent-level 2)
+(setq js2-strict-missing-semi-warning nil)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 ;;   (add-hook 'js-mode-hook 'js2-minor-mode)
