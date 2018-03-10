9fc2a913b40599ed27faed7a28c0defe7bd99408
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-03-10 12:43:51 +0900

    Disable web-mode on '.html', use html-mode

--- config/40-web-mode.el
+++ config/40-web-mode.el
@@ -10,7 +10,7 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+;; (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
 (provide '40-web-mode)
 ;;; 40-web-mode.el ends here
