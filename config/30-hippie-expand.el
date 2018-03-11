67ce2a9bd14bfd325ed62448a3f972d3014ba4db
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-03-11 11:09:39 +0900

    Add some config for hippie-expand

--- config/30-hippie-expand.el
+++ config/30-hippie-expand.el
@@ -1,17 +1,23 @@
-;;-*- coding: utf-8 -*-
+;;; Package --- summary
+;;; -*- coding: utf-8 -*-
+;;; Commentary:
+;;; Code:
 
 (setq hippie-expand-try-functions-list
   '(try-complete-file-name-partially
     try-complete-file-name
-    ;; try-expand-all-abbrevs
-    ;; try-expand-list
-    ;; try-expand-line
+    try-expand-all-abbrevs
+    try-expand-list
+    try-expand-line
     try-expand-dabbrev
     try-expand-dabbrev-all-buffers
     try-expand-dabbrev-from-kill
-    ;; try-complete-lisp-symbol-partially
-    ;; try-complete-lisp-symbol
+    try-complete-lisp-symbol-partially
+    try-complete-lisp-symbol
     ))
 
 (require 'bind-key)
 (bind-key "M-/" 'hippie-expand)
+
+(provide '30-hippie-expand)
+;;; 30-hippie-expand.el ends here
