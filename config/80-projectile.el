a243486ec7a6e0e2d0125f08eda34f0c15c4b1cf
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-03-26 16:50:44 +0900

    Add projectile

---
+++ config/80-projectile.el
@@ -0,0 +1,32 @@
+;;; Package --- summary
+;;; -*- coding: utf-8 -*-
+;;; Commentary:
+;;; Code:
+
+;;; M-x projectile-discover-projects-in-directory
+
+(setq projectile-keymap-prefix (kbd "C-c C-p"))
+(setq projectile-completion-system 'helm)
+(setq projectile-switch-project-action #'projectile-dired)
+
+(add-hook 'persp-mode-projectile-bridge-mode-hook
+          #'(lambda ()
+              (if persp-mode-projectile-bridge-mode
+                  (persp-mode-projectile-bridge-find-perspectives-for-all-buffers)
+                (persp-mode-projectile-bridge-kill-perspectives))))
+(add-hook 'after-init-hook
+          #'(lambda ()
+              (persp-mode-projectile-bridge-mode 1))
+          t)
+
+(add-hook 'ibuffer-hook
+    (lambda ()
+      (ibuffer-projectile-set-filter-groups)
+      (unless (eq ibuffer-sorting-mode 'alphabetic)
+        (ibuffer-do-sort-by-alphabetic))))
+
+(projectile-mode +1)
+(helm-projectile-on)
+
+(provide '80-projectile)
+;;; 80-projectile.el ends here
