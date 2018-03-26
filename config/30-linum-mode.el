172ebd1f8fdccc36e2404b40af78cd048552b2d3
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-03-26 18:04:47 +0900

    Add linum-relative

--- config/30-linum-mode.el
+++ config/30-linum-mode.el
@@ -1,7 +1,17 @@
-;;-*- coding: utf-8 -*-
+;;; Package --- summary
+;;; -*- coding: utf-8 -*-
+;;; Commentary:
+;;; Code:
 
 (setq linum-format "%5d ")
 (add-hook 'prog-mode-hook
           '(lambda ()
              (linum-mode 1)
              (set-face-attribute 'linum nil :height 0.75)))
+
+(require 'bind-key)
+(bind-key "C-c t l" 'linum-mode)
+(bind-key "C-c t r" 'linum-relative-toggle)
+
+(provide '30-linum-mode)
+;;; 30-linum-mode.el ends here
