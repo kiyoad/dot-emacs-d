608c7826ddd0d8847edf518e968e1451499fe02f
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-03-28 21:40:46 +0900

    Refactor bbyac

--- config/30-bbyac.el
+++ config/30-bbyac.el
@@ -1,16 +1,12 @@
-;;-*- coding: utf-8 -*-
+;;; Package --- summary
+;;; -*- coding: utf-8 -*-
+;;; Commentary:
+;;; Code:
 
 ;; http://rubikitch.com/2015/03/12/bbyac/
 
-(require 'browse-kill-ring)
-(require 'bbyac)
-
 (setq bbyac-max-chars 99999)
-(defun bbyac--display-matches--use-ido (orig strlist)
-  (cond ((null (cdr strlist))
-         (car strlist))
-        ((cl-notany #'bbyac--string-multiline-p strlist)
-         (ido-completing-read "Bbyac: " strlist nil t))
-        (t (apply orig strlist))))
-(advice-add 'bbyac--display-matches :around 'bbyac--display-matches--use-ido)
 (bbyac-global-mode 1)
+
+(provide '30-bbyac)
+;;; 30-bbyac.el ends here
