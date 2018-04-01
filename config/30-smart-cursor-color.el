99afec5484a7b07548f9c0b3d2ad96ab9f78bed9
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-04-01 20:54:22 +0900

    Disable smart-cursor-color-mode

--- config/30-smart-cursor-color.el
+++ config/30-smart-cursor-color.el
@@ -1,7 +1,12 @@
-;;-*- coding: utf-8 -*-
+;;; Package --- summary
+;;; -*- coding: utf-8 -*-
+;;; Commentary:
+;;; Code:
 
 ;; http://rubikitch.com/2015/03/26/smart-cursor-color/
-
 ;; emacs -nw では動作しない
-(require 'smart-cursor-color)
-(smart-cursor-color-mode 1)
+
+(smart-cursor-color-mode -1)            ; default off
+
+(provide '30-smart-cursor-color)
+;;; 30-smart-cursor-color.el ends here
