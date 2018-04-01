45a525e8e1c698b67a12601a847b51b2c3d10927
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-04-01 20:56:42 +0900

    Modify ace-isearch delay

--- config/30-ace-isearch.el
+++ config/30-ace-isearch.el
@@ -1,10 +1,16 @@
-;;-*- coding: utf-8 -*-
+;;; Package --- summary
+;;; -*- coding: utf-8 -*-
+;;; Commentary:
+;;; Code:
 
-(require 'ace-isearch)
 (global-ace-isearch-mode 1)
-(setq ace-isearch-jump-delay 0.5)
-(setq ace-isearch-func-delay 2.0)
 (setq ace-isearch-lighter "")
+(setq ace-isearch-jump-delay 1.0)
+(setq ace-isearch-func-delay 3.0)
 ;; (setq ace-isearch-input-length 64)
+
 (require 'bind-key)
 (bind-key "C-i" 'isearch-dabbrev-expand isearch-mode-map)
+
+(provide '30-ace-isearch)
+;;; 30-ace-isearch.el ends here
