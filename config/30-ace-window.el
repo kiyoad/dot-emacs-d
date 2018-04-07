594dc8ee92c8ac424c83c2573a8f31b463b215c7
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-04-07 19:21:43 +0900

    Add ace-window

---
+++ config/30-ace-window.el
@@ -0,0 +1,10 @@
+;;; Package --- summary
+;;; -*- coding: utf-8 -*-
+;;; Commentary:
+;;; Code:
+
+(require 'bind-key)
+(bind-key "M-o" 'ace-window)
+
+(provide '30-ace-window)
+;;; 30-ace-window.el ends here
