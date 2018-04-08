9c19d9fef7f3dd7de4cefa8454ebb616586457c7
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-04-08 18:05:51 +0900

    Remove persp-mode-projectile-bridge-mode

--- config/70-projectile.el
+++ config/70-projectile.el
@@ -12,16 +12,6 @@
   '(:eval (format "[%s]"
                   (projectile-project-name))))
 
-(add-hook 'persp-mode-projectile-bridge-mode-hook
-          #'(lambda ()
-              (if persp-mode-projectile-bridge-mode
-                  (persp-mode-projectile-bridge-find-perspectives-for-all-buffers)
-                (persp-mode-projectile-bridge-kill-perspectives))))
-(add-hook 'after-init-hook
-          #'(lambda ()
-              (persp-mode-projectile-bridge-mode 1))
-          t)
-
 (add-hook 'ibuffer-hook
     (lambda ()
       (ibuffer-projectile-set-filter-groups)
