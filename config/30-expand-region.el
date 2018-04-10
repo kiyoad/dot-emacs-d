b4d50699c0873aa52810551a70af2899360cb617
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-04-10 21:30:54 +0900

    Add expand-region

---
+++ config/30-expand-region.el
@@ -0,0 +1,10 @@
+;;; Package --- summary
+;;; -*- coding: utf-8 -*-
+;;; Commentary:
+;;; Code:
+
+(require 'bind-key)
+(bind-key "M-h" 'er/expand-region)
+
+(provide '30-expand-region)
+;;; 30-expand-region.el ends here
