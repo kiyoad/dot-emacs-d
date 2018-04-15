34c254f9a61dc88ccb66cccfe2dd6fd145d35dfd
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-04-15 13:46:02 +0900

    Remove missing packages

--- config/40-eldoc.el
+++
@@ -1,9 +0,0 @@
-;;-*- coding: utf-8 -*-
-
-(require 'eldoc-extension)
-(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'ielm-mode-hook 'turn-on-eldoc-mode)
-(with-eval-after-load 'eldoc-extension
-  (setq eldoc-idle-delay 0.2)
-  (setq eldoc-minor-mode-string ""))
