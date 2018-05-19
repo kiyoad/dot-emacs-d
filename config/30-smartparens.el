385e2db1ff5aeae3f0b061effb05c69cfe5869c0
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-05-19 17:19:53 +0900

    Add smartparens-strict-mode toggle

--- config/30-smartparens.el
+++ config/30-smartparens.el
@@ -8,5 +8,8 @@
 (smartparens-global-mode +1)
 (add-hook 'prog-mode-hook 'turn-on-smartparens-strict-mode)
 
+(require 'bind-key)
+(bind-key "C-c t s" 'smartparens-strict-mode)
+
 (provide '30-smartparens)
 ;;; 30-smartparens.el ends here
