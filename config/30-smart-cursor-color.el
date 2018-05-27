7286a900c465d04ce194d950aef4057125e6af2e
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-05-27 09:19:34 +0900

    Change smart-cursor-color-mode default to on

--- config/30-smart-cursor-color.el
+++ config/30-smart-cursor-color.el
@@ -6,7 +6,7 @@
 ;; http://rubikitch.com/2015/03/26/smart-cursor-color/
 ;; emacs -nw では動作しない
 
-(smart-cursor-color-mode -1)            ; default off
+(smart-cursor-color-mode 1)
 
 (provide '30-smart-cursor-color)
 ;;; 30-smart-cursor-color.el ends here
