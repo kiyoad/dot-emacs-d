74d337687fad453dfde00be8a2a379f96eab1e6c
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2018-10-21 20:55:31 +0900

    Remove which-function-mode

--- config/30-which-function-mode.el
+++
@@ -1,9 +0,0 @@
-;;; Package --- summary
-;;; -*- coding: utf-8 -*-
-;;; Commentary:
-;;; Code:
-
-(which-function-mode 1)
-
-(provide '30-which-function-mode)
-;;; 30-which-function-mode.el ends here
