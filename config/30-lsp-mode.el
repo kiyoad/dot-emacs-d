58730276487a905b4f48db667ddde852f80e421c
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2019-01-03 09:13:48 +0900

    Use lsp-python instead of jedi

---
+++ config/30-lsp-mode.el
@@ -0,0 +1,11 @@
+;;; Package --- summary
+;;; -*- coding: utf-8 -*-
+;;; Commentary:
+;;; Code:
+
+(require 'lsp-mode)
+(require 'lsp-ui)
+(require 'company-lsp)
+
+(provide '30-lsp-mode)
+;;; 30-lsp-mode.el ends here
---
+++ config/40-python.el
@@ -0,0 +1,9 @@
+;;; Package --- summary
+;;; -*- coding: utf-8 -*-
+;;; Commentary:
+;;; Code:
+
+(add-hook 'python-mode-hook #'lsp)
+
+(provide '40-python.el)
+;;; 40-python.el ends here
