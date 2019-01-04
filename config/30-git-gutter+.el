b1d8586c23b2d762da4de30da5d9168ef4e01b96
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2019-01-04 13:58:13 +0900

    Use git-gutter+-mode-map instead of global map

--- config/30-git-gutter+.el
+++ config/30-git-gutter+.el
@@ -8,8 +8,8 @@
 (global-git-gutter+-mode)
 
 (require 'bind-key)
-(bind-key "C-c t g" 'git-gutter+-toggle-fringe)
-(bind-key "C-x v SPC" 'git-gutter+-show-hunk-inline-at-point)
+(bind-key "C-c t g" 'git-gutter+-toggle-fringe git-gutter+-mode-map)
+(bind-key "C-x v SPC" 'git-gutter+-show-hunk-inline-at-point git-gutter+-mode-map)
 
 (provide '30-git-gutter+)
 ;;; 30-git-gutter+.el ends here
