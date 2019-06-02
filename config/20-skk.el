148ddc45f55b546f3d101597a32623cf3259832e
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2019-06-02 11:25:05 +0900

    Change skk settings and add M-\ for toggle-input-method

--- config/20-skk.el
+++ config/20-skk.el
@@ -1,16 +1,24 @@
-;;-*- coding: utf-8 -*-
+;;; Package --- summary
+;;; -*- coding: utf-8 -*-
+;;; Commentary:
+;;; Code:
 
-;; http://qiita.com/ta2gch/items/c5c7115e0ac3f8273664
+;; https://skk-dev.github.io/ddskk/skk.html
 
 (setq skk-user-directory (concat user-emacs-directory "ddskk"))
 ;; https://raw.githubusercontent.com/skk-dev/ddskk/master/etc/SKK.tut
 (setq skk-tut-file (concat user-emacs-directory "ddskk/SKK.tut"))
 ;; M-x skk-get
 (setq skk-large-jisyo (concat user-emacs-directory "skk-get-jisyo/SKK-JISYO.L"))
-(require 'skk)
-(setq default-input-method "japanese-skk") ;; emacs上での日本語入力にskkをつかう
+
 (add-hook 'skk-load-hook
           (lambda ()
-            (require 'context-skk))) ;; 文脈に応じた自動モード切り替え
-(require 'skk-hint)                  ;; 候補の絞り込み
-(require 'skk-study)                 ;; 変換学習機能の追加
+            (require 'context-skk)))
+
+(setq default-input-method "japanese-skk") ;; emacs上での日本語入力にskkをつかう
+
+(require 'bind-key)
+(bind-key "M-\\" 'toggle-input-method)
+
+(provide '20-skk)
+;;; 20-skk.el ends here
