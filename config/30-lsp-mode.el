e73097b87dc978657408ad47d3ff5a38f638a940
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2019-06-02 09:06:12 +0900

    Add lsp for bash

--- config/30-lsp-mode.el
+++ config/30-lsp-mode.el
@@ -7,5 +7,7 @@
 (require 'lsp-ui)
 (require 'company-lsp)
 
+(add-to-list 'lsp-language-id-configuration '(sh-mode . "bash"))
+
 (provide '30-lsp-mode)
 ;;; 30-lsp-mode.el ends here
---
+++ config/40-sh-mode.el
@@ -0,0 +1,9 @@
+;;; Package --- summary
+;;; -*- coding: utf-8 -*-
+;;; Commentary:
+;;; Code:
+
+(add-hook 'sh-mode-hook #'lsp)
+
+(provide '40-sh-mode)
+;;; 40-sh-mode.el ends here
