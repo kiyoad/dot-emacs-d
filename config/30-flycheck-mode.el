f8e8fd5eada5a0ae123b286ddcefa474e3a6b88d
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2019-06-16 08:59:51 +0900

    Revert flycheck-popup-tip-mode in flycheck-mode

--- config/30-flycheck-mode.el
+++ config/30-flycheck-mode.el
@@ -8,5 +8,8 @@
 (require 'flycheck)
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
+(with-eval-after-load 'flycheck
+  (add-hook 'flycheck-mode-hook 'flycheck-popup-tip-mode))
+
 (provide '30-flycheck-mode)
 ;;; 30-flycheck-mode.el ends here
