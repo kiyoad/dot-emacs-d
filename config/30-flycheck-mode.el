1931ec926ee5ad5cb4c90fccf3f43eb6924f6513
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2019-06-15 17:45:22 +0900

    Use python2 tools instead of python3 them

--- config/30-flycheck-mode.el
+++ config/30-flycheck-mode.el
@@ -6,16 +6,6 @@
 ;;; C-c ! v runs the command flycheck-verify-setup
 
 (require 'flycheck)
-
-(defun my-use-python3-instead-of-python2 (x)
-  "Replace input (as X)."
-  (cond ((string= x "python") "python3")
-        (t x)))
-
-(with-eval-after-load 'flycheck
-  (advice-add 'flycheck-checker-default-executable :filter-return #'my-use-python3-instead-of-python2)
-  (add-hook 'flycheck-mode-hook 'flycheck-popup-tip-mode))
-
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 (provide '30-flycheck-mode)
