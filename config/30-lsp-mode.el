b3d2a74178121183d07c64837b1e5c3d036ab360
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2019-06-15 18:24:53 +0900

    Use flycheck instead of flymake in lsp

--- config/30-lsp-mode.el
+++ config/30-lsp-mode.el
@@ -8,6 +8,11 @@
 (require 'company-lsp)
 
 (add-to-list 'lsp-language-id-configuration '(sh-mode . "bash"))
+(setq lsp-prefer-flymake nil)
+(setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
+(setq lsp-ui-peek-always-show t)
+(setq lsp-ui-doc-header t)
+(setq lsp-ui-doc-include-signature t)
 
 (provide '30-lsp-mode)
 ;;; 30-lsp-mode.el ends here
