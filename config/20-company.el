ff2e454f40778e0c4e6f6cdcbf393db1c09f9786
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2019-06-15 17:33:39 +0900

    Add useful keybindings in company

--- config/20-company.el
+++ config/20-company.el
@@ -20,5 +20,14 @@
 
 (bind-key "SPC" 'my-company-abort-space company-active-map)
 
+(bind-key "C-n" 'company-select-next company-active-map)
+(bind-key "C-p" 'company-select-previous company-active-map)
+(bind-key "C-s" 'company-filter-candidates company-active-map)
+(bind-key "C-i" 'company-complete-selection company-active-map)
+(bind-key "<tab>" 'company-complete-selection company-active-map)
+
+(bind-key "C-n" 'company-select-next company-search-map)
+(bind-key "C-p" 'company-select-previous company-search-map)
+
 (provide '20-company)
 ;;; 20-company.el ends here
