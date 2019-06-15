b6f3abb731b79d92d07f16d122a48c4343385601
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2019-06-15 18:26:41 +0900

    Disable auto hooks for lsp in c-mode
    When some C source files are automatically read at startup by
    persp-mode, indexing of those files by ccls is performed in a
    burst, so it waits for a long time until startup is completed.
    
    Therefore, lsp-mode for C source files are performed manually for
    each file.

--- config/40-c-mode.el
+++ config/40-c-mode.el
@@ -4,9 +4,20 @@
 ;;; Code:
 
 (require 'ccls)
-(add-hook 'c-mode-hook #'lsp)
-(add-hook 'c++-mode-hook #'lsp)
-(add-hook 'objc-mode-hook #'lsp)
+(setq ccls-args '("--init={\"index\": {\"initialBlacklist\": [\".\"], \"threads\":0}}"))
+(setq ccls-sem-highlight-method 'font-lock)
+(ccls-use-default-rainbow-sem-highlight)
+
+;; When some C source files are automatically read at startup by
+;; persp-mode, indexing of those files by ccls is performed in a
+;; burst, so it waits for a long time until startup is completed.
+
+;; Therefore, lsp-mode for C source files are performed manually for
+;; each file.
+
+;; (add-hook 'c-mode-hook #'lsp)
+;; (add-hook 'c++-mode-hook #'lsp)
+;; (add-hook 'objc-mode-hook #'lsp)
 
 (provide '40-c-mode.el)
 ;;; 40-c-mode.el ends here
