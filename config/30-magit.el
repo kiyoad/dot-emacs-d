45a9f583e9d5c9233833416e7090c8793f43ca62
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2019-11-05 17:39:59 +0900

    Change call functions for log and show-refs

--- config/30-magit.el
+++ config/30-magit.el
@@ -1,10 +1,15 @@
-;;-*- coding: utf-8 -*-
+;;; Package --- summary
+;;; -*- coding: utf-8 -*-
+;;; Commentary:
+;;; Code:
 
 (require 'bind-key)
 (require 'magit)
 (bind-key "C-x v d" 'magit-status)
-(bind-key "C-x v L" 'magit-log-popup)
+(bind-key "C-x v L" 'magit-log)
 (bind-key "C-x v z" 'magit-stash)
-(bind-key "C-x v B" 'magit-show-refs-popup)
+(bind-key "C-x v B" 'magit-show-refs)
 (require 'mo-git-blame)
 (bind-key "C-x v g" 'mo-git-blame-current)
+
+;;; 30-magit.el ends here
