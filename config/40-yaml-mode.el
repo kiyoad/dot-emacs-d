919f6d735911311b2fb8579d71901c634ba4500b
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2020-01-01 19:42:12 +0900

    Use lsp in yaml-mode

--- config/40-yaml-mode.el
+++ config/40-yaml-mode.el
@@ -3,7 +3,9 @@
 ;;; Commentary:
 ;;; Code:
 
+(require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
+(add-hook 'yaml-mode-hook #'lsp-deferred)
 
 (provide '40-yaml-mode)
 ;;; 40-yaml-mode.el ends here
