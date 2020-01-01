17012d6a146af65e1df15a762faae42baa6565d9
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2020-01-01 19:41:15 +0900

    Use lsp in dockerfile-mode

--- config/40-dockerfile-mode.el
+++ config/40-dockerfile-mode.el
@@ -1,3 +1,10 @@
-;;-*- coding: utf-8 -*-
+;;; Package --- summary
+;;; -*- coding: utf-8 -*-
+;;; Commentary:
+;;; Code:
 
 (require 'dockerfile-mode)
+(add-hook 'dockerfile-mode-hook #'lsp-deferred)
+
+(provide '40-dockerfile-mode)
+;;; 40-dockerfile-mode.el ends here
