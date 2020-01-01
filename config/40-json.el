f40039e4ed23c4f22da980ad58a306d03c1e43b2
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2020-01-01 19:42:52 +0900

    Use lsp in json-mode

--- config/40-json.el
+++ config/40-json.el
@@ -1,4 +1,11 @@
-;;-*- coding: utf-8 -*-
+;;; Package --- summary
+;;; -*- coding: utf-8 -*-
+;;; Commentary:
+;;; Code:
 
 (require 'json-mode)
 (require 'json-reformat)
+(add-hook 'json-mode-hook #'lsp-deferred)
+
+(provide '40-json)
+;;; 40-json.el ends here
