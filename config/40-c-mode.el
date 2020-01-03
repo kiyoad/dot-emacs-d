b22a9f8dd17534eed80c6ab70371a7a1fedbd581
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2020-01-03 15:41:14 +0900

    Use ccls again

--- config/40-c-mode.el
+++ config/40-c-mode.el
@@ -3,14 +3,22 @@
 ;;; Commentary:
 ;;; Code:
 
-(setq lsp-clients-clangd-executable "/usr/lib/llvm-8/bin/clangd")
-(setq company-clang-executable "/usr/bin/clang-8")
-(add-hook 'c-mode-hook #'lsp-deferred)
-(add-hook 'c++-mode-hook #'lsp-deferred)
-(add-hook 'objc-mode-hook #'lsp-deferred)
+;; ccls を使う場合は以下を無効にする
+;; (setq lsp-clients-clangd-executable "/usr/lib/llvm-8/bin/clangd")
 
-;; flymake を無効にするのは以下
+(require 'ccls)
+(setq ccls-executable "/usr/local/bin/ccls")
+
+(setq company-clang-executable "/usr/bin/clang-8")
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (flycheck-mode -1)
+            (lsp-deferred)))
+
+;; flycheck を使う場合は以下を有効にする。
+;; (setq lsp-prefer-flymake nil)
 ;; (setq-default flycheck-disabled-checkers '(c/c++-clang c/c++-cppcheck c/c++-gcc))
+;; flycheck でも結局はinclude pathの問題がある。
 
 ;; flymake を実行できるようにするためには、
 ;; Makeflle に以下のような設定が必要
