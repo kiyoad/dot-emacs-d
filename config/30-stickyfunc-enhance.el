1a153bf8fa2c49ccba4a25844f95c2a3c85b6281
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2020-04-08 19:29:37 +0900

    Add stickyfunc-enhance

---
+++ config/30-stickyfunc-enhance.el
@@ -0,0 +1,9 @@
+;;; Package --- summary
+;;; -*- coding: utf-8 -*-
+;;; Commentary:
+;;; Code:
+
+(require 'stickyfunc-enhance)
+(add-to-list 'semantic-default-submodes 'global-semantic-stickyfunc-mode)
+
+;;; 30-stickyfunc-enhance.el ends here
