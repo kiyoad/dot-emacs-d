a1457cb4da7fcf49f4ed4244bc48d34563cd3021
KIYOHIRO ADACHI <kiyoad@da2.so-net.ne.jp>
2020-08-31 10:07:50 +0900

    Change swiper keybind to C-u C-u C-s

--- config/30-swiper.el
+++ config/30-swiper.el
@@ -4,8 +4,15 @@
 ;;; Code:
 
 (require 'swiper)
+
+(defun my-isearch-or-swiper (prefix)
+  (interactive "P")
+  (if (equal prefix '(16))
+      (swiper)
+    (isearch-forward prefix)))
+
 (require 'bind-key)
-(bind-key "C-r" 'swiper)
+(bind-key "C-s" 'my-isearch-or-swiper)
 
 (setq swiper-include-line-number-in-search t)
 
